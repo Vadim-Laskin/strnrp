c284250fdc94abfa95b9dfbee36f1889ab3720a0
Vadim-Laskin <140655996+Vadim-Laskin@users.noreply.github.com>
2024-11-16 12:48:36 +0700

    Create gekas.php

---
+++ gekas.php
@@ -0,0 +1,1 @@
+[{"showOne":true,"numberOne":1,"bonusOne":true,"nameOne":"Paradox","colorOne":"fff1c127","minOne":9,"maxOne":1000,"showTwo":false,"numberTwo":2,"bonusTwo":true,"nameTwo":"Florida #2","colorTwo":"0068FF","minTwo":150,"maxTwo":1000}]
