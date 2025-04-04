bce9b3af2a9b8f1e92b4f5074a3d7d73e0b78da4
Vadim-Laskin <140655996+Vadim-Laskin@users.noreply.github.com>
2025-04-04 18:28:36 +0700

    Delete gekas.php

--- gekas.php
+++
@@ -1,1 +0,0 @@
-[{"showOne":true,"numberOne":1,"bonusOne":true,"nameOne":"Paradox","colorOne":"fff1c127","minOne":9,"maxOne":1000,"showTwo":false,"numberTwo":2,"bonusTwo":true,"nameTwo":"Florida #2","colorTwo":"0068FF","minTwo":150,"maxTwo":1000}]
